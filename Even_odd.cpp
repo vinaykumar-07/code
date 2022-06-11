61bdeadac4588a79aed1971851fa238f25375db5
vinaykumar-07 <88262239+vinaykumar-07@users.noreply.github.com>
2022-06-11 15:50:24 +0530

    Basic code

---
+++ Even_odd.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n % 2 == 0)
+		cout << n << " even number" << endl;
+	else
+		cout << n << " odd number" << endl;
+	return 0;
+}
