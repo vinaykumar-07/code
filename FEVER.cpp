6e93220f82b1f2051c42e252f01c23b7762041f5
Vinay Kumar <88262239+vinaykumar-07@users.noreply.github.com>
2022-09-08 23:16:08 +0530

    FEVER Codechef

---
+++ FEVER.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        if (x >= 99)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
