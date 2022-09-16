1063a4afb096e9bc3a133df3042c0632d2fc1282
Vinay Kumar <88262239+vinaykumar-07@users.noreply.github.com>
2022-09-16 18:52:37 +0530

    Nearest Exit

---
+++ Nearest_Exit.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	 int t;
+	 cin>>t;
+	 while(t--)
+	 {
+	     int x;
+	     cin>>x;
+	     if(x<=50)
+	     cout<<"LEFT"<<endl;
+	     else
+	     cout<<"RIGHT"<<endl;
+	 }
+	return 0;
+}
