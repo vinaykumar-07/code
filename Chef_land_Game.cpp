e51031ab6959991b9dc7f5882344000438e0c46e
Vinay Kumar <88262239+vinaykumar-07@users.noreply.github.com>
2022-09-16 18:48:08 +0530

    Chefland Games

---
+++ Chef_land_Game.cpp
@@ -0,0 +1,17 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	 int t;
+	 cin>>t;
+	 while(t--)
+	 {
+	     int r1,r2,r3,r4;
+	     cin>>r1>>r2>>r3>>r4;
+	    if(r1==0&&r2==0&&r3==0&&r4==0){ 
+	    	 cout<<"IN"<<endl;}
+	    else
+	    	 cout<<"OUT"<<endl;
+	 }
+	return 0;
+}		
